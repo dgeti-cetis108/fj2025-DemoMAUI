a55043e428fec556e403fab7c47e1443602d6abf
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-05-02 12:50:53 -0700

    Agregar funcionalidad de traducción en la página HolaMundo

--- DemoMAUI/HolaMundo.xaml.cs
+++ DemoMAUI/HolaMundo.xaml.cs
@@ -2,8 +2,25 @@ namespace DemoMAUI;
 
 public partial class HolaMundo : ContentPage
 {
+	public Button MiBoton { get; set; }
+	private string IdiomaActual {get;set;} = "en";
 	public HolaMundo()
 	{
 		InitializeComponent();
+	}
+
+	private void traducir(object sender, EventArgs e)
+	{
+		if (IdiomaActual == "en") {
+			LabelMensaje.Text = "Bienvenido a .NET MAUI";
+			ButtonTraducir.Text = "Translate";
+			IdiomaActual = "es";
+		}
+		else
+		{
+			LabelMensaje.Text = "Welcome to .NET MAUI";
+			ButtonTraducir.Text = "Traducir";
+			IdiomaActual = "en";
+		}
 	}
 }
